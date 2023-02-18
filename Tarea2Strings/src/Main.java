5679d64ac4e9dd7733fc7ba105d3a88e76893d74
Erick-Baco <79772082+Erick-Baco@users.noreply.github.com>
2023-02-18 11:27:33 -0600

    Add files via upload
    entrega.

---
+++ Tarea2Strings/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String data = null;
+
+        data= sc.nextLine();
+        String parts[] = data.split(" ");
+
+        String name = parts[0];
+        String profesion = parts [1];
+        String country = parts [2];
+
+        System.out.println("Nombre: " + name);
+        System.out.println("Profesión: " + profesion);
+        System.out.println("País: " + country);
+
+    }
+}
